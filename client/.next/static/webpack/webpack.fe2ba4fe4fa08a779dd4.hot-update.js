195f63d94145c5ac8bcf184e4aaf767778dc312e
Thomas Billington <thombillington@gmail.com>
2021-05-20 19:12:21 -0700

    oops

---
+++ client/.next/static/webpack/webpack.fe2ba4fe4fa08a779dd4.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "35910cd5e761cd44c925"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay5mZTJiYTRmZTRmYTA4YTc3OWRkNC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjM1OTEwY2Q1ZTc2MWNkNDRjOTI1XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
---
+++ client/components/TopNav.js
@@ -0,0 +1,9 @@
+const TopNav = () => {
+    return (
+        <>
+            <p>Top Nav</p>
+        </>
+    )
+}
+
+export default TopNav;
